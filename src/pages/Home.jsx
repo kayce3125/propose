5df5d975cfcfd129dea7e296b0ebc52bd85a2cc9
kayce3125 <75183770+kayce3125@users.noreply.github.com>
2024-03-20 02:50:54 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,26 +8,7 @@ const Home = ({ className = '' }) => (
         <Container>
             <Row className="hero_content justify-content-between py-5">
                 <Col md={7} lg={6} className="hero_text-wrapper">
-                    <div className="hero_text">
-                        <h1 className="hero_text-title mb-2">
-                            <font className="text-danger">Propose</font> Your Valentine
-                        </h1>
-                        <p className="hero_text-description">
-                            Enter your valentine's name and generate a propose link.
-                        </p>
-
-                        <ProposeForm className="hero_text-form mt-4 mt-md-5" />
-                        <p className="hero_credit">
-                            developed by{' '}
-                            <a
-                                href="https://tonmoydeb.com"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                @tonmoydeb
-                            </a>
-                        </p>
-                    </div>
+                    
                 </Col>
                 <Col md={5} className="hero_media-wrapper">
                     <div className="hero_media">
